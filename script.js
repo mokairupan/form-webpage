90e3f8744fe85b14e65e39004d2cb8d7f0212e8a
mokairupan <mokankirupan@gmail.com>
2024-10-21 18:29:17 +0530

    first_commit

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $("#signup").validate({
+        rules: {
+            fname:{
+                required: true,
+                minlength: 5,
+                maxlength: 10,
+            },
+            sname:{
+                required: true,
+            },
+            day:{
+                required: true,
+            },
+            month:{
+                required: true,
+            },
+            year:{
+                required: true,
+            },
+            mail:{
+                required: true,
+            },
+            mobile:{
+                required: true,
+            },
+            password: {
+                required: true,
+                minlength: 8,
+            },
+            Re_password: {
+                required: true,
+                minlength: 8,
+                value:$("password").value,
+            }
+        }
+    })
+});
